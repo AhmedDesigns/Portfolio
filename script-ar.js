656a4d5ae34a9f17da0eaf9cea7a61dc37606e45
AhmedDesigns <148201073+AhmedDesigns@users.noreply.github.com>
2025-11-22 12:32:25 +0200

    Update script-ar.js

--- script-ar.js
+++ script-ar.js
@@ -1,4 +1,3 @@
-//==================== toggle icon navbar ====================//
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,35 +6,27 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
 
-//==================== scroll sections active link ====================//
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   let top = window.scrollY;
-  
   sections.forEach(sec => {
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
-    
-    if (top >= offset && top < offset + height) {
+    if(top >= offset && top < offset + height){
       navlinks.forEach(link => link.classList.remove('active'));
       let activeLink = document.querySelector('header nav a[href*=' + id + ']');
-      if (activeLink) activeLink.classList.add('active');
+      if(activeLink) activeLink.classList.add('active');
     }
   });
-  
-  //==================== sticky navbar ====================//
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
-  
-  //==================== remove toggle icon and navbar when click navbar link (scroll) ====================//
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
 
-//==================== scroll reveal ====================//
 ScrollReveal({
   distance: '80px',
   duration: 2000,
@@ -47,9 +38,7 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-container, .co
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'right' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-//==================== typed js بالعربي ====================//
 const typed = new Typed('.multiple-text', {
-  const typed = new Typed('.multiple-text', {
   strings: ['مصمم ويب', 'مطور ويب', 'مصمم واجهات', 'خبير تجارة إلكترونية'],
   typeSpeed: 100,
   backSpeed: 100,
