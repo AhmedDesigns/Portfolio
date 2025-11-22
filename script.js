56c0c32add5858165d30f6ec0d6221289985f134
AhmedDesigns <148201073+AhmedDesigns@users.noreply.github.com>
2025-11-22 11:47:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//==================== toggle icon navbar ====================//
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,6 +7,7 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
 
+//==================== scroll sections active link ====================//
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -19,18 +21,21 @@ window.onscroll = () => {
     
     if (top >= offset && top < offset + height) {
       navlinks.forEach(link => link.classList.remove('active'));
-      let activeLink = document.querySelector('header nav a[href="#' + id + '"]');
+      let activeLink = document.querySelector('header nav a[href*=' + id + ']');
       if (activeLink) activeLink.classList.add('active');
     }
   });
   
+  //==================== sticky navbar ====================//
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
   
+  //==================== remove toggle icon and navbar when click navbar link (scroll) ====================//
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
 
+//==================== scroll reveal ====================//
 ScrollReveal({
   distance: '80px',
   duration: 2000,
@@ -42,8 +47,9 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-container, .co
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
+//==================== typed js ====================//
 const typed = new Typed('.multiple-text', {
-  strings: ['مصمم ويب', 'مطور ويب', 'مصمم UI/UX', 'خبير تجارة إلكترونية'],
+  strings: ['Web Designer', 'Web Developer', 'UI/UX Designer', 'E-commerce Expert'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
