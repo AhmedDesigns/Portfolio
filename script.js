8a3df1b9013d82d224a0ea99d42cc147ec3da843
AhmedDesigns <148201073+AhmedDesigns@users.noreply.github.com>
2025-11-22 14:48:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //==================== typed js ====================//
 const typed = new Typed('.multiple-text', {
-  strings: ['Web Designer', 'Web Developer', 'UI/UX Designer', 'E-commerce Expert'],
+  strings: ['مصمم ويب', 'مطور ويب', 'مصمم UI/UX', 'خبير تجارة إلكترونية'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
